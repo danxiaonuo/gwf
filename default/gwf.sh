43e6f70d1d07511fdeb2a3b9a39c4e4be8a3ea1c
danxiaonuo <danxiaonuo@danxiaonuo.me>
2021-12-23 11:03:37 +0800

    Update gwf.sh

--- default/gwf.sh
+++ default/gwf.sh
@@ -1,3 +1,4 @@
+# CN IP
 curl -s -m 3 --retry-delay 3 --retry 3 -k -4 --header 'cache-control: no-cache' --url 'https://ispip.clang.cn/all_cn.txt' > china_ipv4.txt
 curl -s -m 3 --retry-delay 3 --retry 3 -k -4 --header 'cache-control: no-cache' --url 'https://ispip.clang.cn/all_cn_ipv6.txt' > china_ipv6.txt
 cat china_ipv4.txt china_ipv6.txt > china_ip.txt
@@ -16,3 +17,6 @@ curl -sS https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/
 
 cat /tmp/temp_gfwlist1 /tmp/temp_gfwlist2 /tmp/temp_gfwlist3  | \
     sort -u | sed 's/^\.*//g' > gfwlist.txt
+
+# MMDB库
+curl -s -m 3 --retry-delay 3 --retry 3 -k -4 --header 'cache-control: no-cache' --url 'https://raw.githubusercontent.com/alecthw/mmdb_china_ip_list/release/Country.mmdb' > Country.mmdb
