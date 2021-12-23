4125a32ff153aba4c824501f1b7a0d408e1b45df
danxiaonuo <danxiaonuo@danxiaonuo.me>
2021-12-23 10:54:54 +0800

    Update purge.sh

--- default/purge.sh
+++ default/purge.sh
@@ -1,3 +1,5 @@
 curl -s -m 3 --retry-delay 3 --retry 3 -k -4 --header 'cache-control: no-cache' --url 'https://purge.jsdelivr.net/gh/danxiaonuo/gwf/china_ip.txt'
 curl -s -m 3 --retry-delay 3 --retry 3 -k -4 --header 'cache-control: no-cache' --url 'https://purge.jsdelivr.net/gh/danxiaonuo/gwf/china_ipv4.txt'
 curl -s -m 3 --retry-delay 3 --retry 3 -k -4 --header 'cache-control: no-cache' --url 'https://purge.jsdelivr.net/gh/danxiaonuo/gwf/china_ipv6.txt'
+curl -s -m 3 --retry-delay 3 --retry 3 -k -4 --header 'cache-control: no-cache' --url 'https://purge.jsdelivr.net/gh/danxiaonuo/gwf/gfwlist.txt'
+
