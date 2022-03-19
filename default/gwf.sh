8b29a301a36f79756d906544a65314f512a62df0
danxiaonuo <danxiaonuo@danxiaonuo.me>
2022-03-19 20:31:07 +0800

    Update gwf.sh

--- default/gwf.sh
+++ default/gwf.sh
@@ -1,6 +1,8 @@
 # CN IP
-curl -s -m 3 --retry-delay 3 --retry 3 -k -4 --header 'cache-control: no-cache' --url 'https://ispip.clang.cn/all_cn.txt' > china_ipv4.txt
-curl -s -m 3 --retry-delay 3 --retry 3 -k -4 --header 'cache-control: no-cache' --url 'https://ispip.clang.cn/all_cn_ipv6.txt' > china_ipv6.txt
+# curl -s -m 3 --retry-delay 3 --retry 3 -k -4 --header 'cache-control: no-cache' --url 'https://ispip.clang.cn/all_cn.txt' > china_ipv4.txt
+# curl -s -m 3 --retry-delay 3 --retry 3 -k -4 --header 'cache-control: no-cache' --url 'https://ispip.clang.cn/all_cn_ipv6.txt' > china_ipv6.txt
+curl -s -m 3 --retry-delay 3 --retry 3 -k -4 --header 'cache-control: no-cache' --url 'https://raw.githubusercontent.com/17mon/china_ip_list/master/china_ip_list.txt' > china_ipv4.txt
+curl -s -m 3 --retry-delay 3 --retry 3 -k -4 --header 'cache-control: no-cache' --url 'https://bgp.space/china6.html' | grep "^[0-9a-z:\/]*<br>" | sed "s/<br>//g" > china_ipv6.txt
 cat china_ipv4.txt china_ipv6.txt > china_ip.txt
 
 # GFW List
@@ -17,6 +19,13 @@ curl -sS https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/
 
 cat /tmp/temp_gfwlist1 /tmp/temp_gfwlist2 /tmp/temp_gfwlist3  | \
     sort -u | sed 's/^\.*//g' > gfwlist.txt
+    
+# smartdns
+cat gfwlist.txt | sed 's/^/\./g' > smartdns_gfw_domain.conf
+sed -i 's/^/nameserver \//' smartdns_gfw_domain.conf
+sed -i 's/$/\/GFW/' smartdns_gfw_domain.conf
+echo "# GFW List" > smartdns_tmp.conf
+cat smartdns_tmp.conf smartdns_gfw_domain.conf > smartdns_gfw_domain.conf
 
 # MMDB库
 curl -s -m 3 --retry-delay 3 --retry 3 -k -4 --header 'cache-control: no-cache' --url 'https://raw.githubusercontent.com/alecthw/mmdb_china_ip_list/release/Country.mmdb' > Country.mmdb
